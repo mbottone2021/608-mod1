67946dd612a3d16295739a4e76559a1d6de8813c
mbottone2021 <77653069+mbottone2021@users.noreply.github.com>
2021-01-18 23:41:46 -0600

    Create bonus.py

---
+++ bonus.py
@@ -0,0 +1,7 @@
+>>> scores = [85,100,100,98,76,85,95,66,49,100,100,88,98,81,96,50,100]
+>>> print(min(scores))
+49
+>>> print(max(scores))
+100
+>>> print('Range:',min(scores),'-',max(scores))
+('Range:', 49, '-', 100)
